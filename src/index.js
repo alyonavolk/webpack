64b892e8c8d9377f4e02f0d1279125b7a52846e1
Alyona Volkova <alyonavolk30@gmail.com>
2023-05-27 23:29:39 +0500

    add css, scss and postcss to project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './styles/index.scss';
+
 const elvenShiledRecipe = {
     leatherScripts: 2,
     ironIngot: 1,
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
     module: {
         rules: [
@@ -7,12 +9,26 @@ module.exports = {
                 use: {
                     loader: "babel-loader"
                 }
-            }
+            },
+            {
+                test: /\.(sc|c)ss$/i, // /\.s?css$/i
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ]
+            },
         ]
     },
+
+    plugins: [
+        new MiniCssExtractPlugin(),
+    ],
 
     devtool: "source-map",
     devServer: {
         static: "./dist",
+        // hot: true,
     }
 }
